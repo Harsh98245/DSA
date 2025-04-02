36a564650309864c18898f96c5794964c72d071a
Harsh98245 <162619122+Harsh98245@users.noreply.github.com>
2025-04-02 01:59:57 -0400

    Time: 6 ms (70.22%), Space: 13.7 MB (64.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,12 +1,10 @@
 class Solution:
     def majorityElement(self, nums):
-        candidate = None  # No candidate at the start
-        count = 0  # Initial count is 0
-
-        # Iterate through each number in the list
+        candidate = None
+        count = 0
         for num in nums:
             if count == 0:
-                candidate = num  # Set the current number as the candidate
-            count += (1 if num == candidate else -1)  # If it matches, increase count, else decrease
+                candidate = num
+            count += (1 if num == candidate else -1)
 
-        return candidate  # The final candidate is the majority element
+        return candidate
