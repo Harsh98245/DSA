47eb25667f949bb590333db78440d7da3eb5fd3e
Harsh98245 <162619122+Harsh98245@users.noreply.github.com>
2025-04-02 01:59:07 -0400

    Time: 14 ms (32.68%), Space: 13.8 MB (31.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,15 +1,12 @@
 class Solution:
     def majorityElement(self, nums):
-        candidate = None
-        count = 0
+        candidate = None  # No candidate at the start
+        count = 0  # Initial count is 0
 
-        # Boyer-Moore Voting Algorithm
+        # Iterate through each number in the list
         for num in nums:
             if count == 0:
-                candidate = num
-            if(candidate==num):
-                count+=1
-            else:
-                count-=1
+                candidate = num  # Set the current number as the candidate
+            count += (1 if num == candidate else -1)  # If it matches, increase count, else decrease
 
-        return candidate
+        return candidate  # The final candidate is the majority element
